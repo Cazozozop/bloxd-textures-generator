84a0ea80bf6941a3f5ab673fa3c3fedadf8ad17e
Cazozozop <cazozop030@gmail.com>
2024-11-13 16:48:00 +0100

    Delete textures.js

--- textures.js
+++
@@ -1,9 +0,0 @@
-// Exemple de chargement d'une texture PNG et d'affichage sur un canvas
-const textureImage = new Image();
-textureImage.src = 'textures/epée.png'; // Remplace par l'URL de la texture sur GitHub
-
-textureImage.onload = function() {
-    const canvas = document.getElementById('textureCanvas');
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(textureImage, 0, 0); // Affiche la texture dans le canvas
-};
